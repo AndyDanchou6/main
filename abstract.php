317a76d3a411ed3f246f10f9dd30d4aee838423c
AndyDanchou6 <dvoid367@gmail.com>
2023-05-13 12:53:37 +0800

    database update

--- abstract.php
+++ abstract.php
@@ -2,5 +2,11 @@
 
 abstract class Database {
     protected $conn;
-    protected $db = "lina"
+    protected $db = "lina";
+
+    abstract public function init() {
+        $this->conn = new mysqli('localhost', 'root', '');
+        $this->conn->query("CREATE DATABASE IF NOT EXISTS". $this->db);
+        $this->conn = new mysqli('localhost', 'root', '', $this->db);
+    }
 }
